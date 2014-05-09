cf587ad5ddb62cfc4729ef03dd93f4fe3a7630c3
David Zech <david.zech@gmail.com>
2014-05-08 21:50:56 -0700

    stringify

--- bot_modules/pantsu/index.js
+++ bot_modules/pantsu/index.js
@@ -91,7 +91,7 @@ function pantsuCommand(irc, from, to, url)
 						else
 						{
 							irc.say(utils.reply(from,to), "Upload failed");
-							console.log("Upload failed: " + res);
+							console.log("Upload failed: " + JSON.stringify(res));
 						}
 					});
 			}
@@ -108,7 +108,7 @@ function pantsuCommand(irc, from, to, url)
 				else
 				{
 					irc.say(utils.reply(from,to), "Upload failed");
-					console.log("Upload failed: " + res);
+					console.log("Upload failed: " + JSON.stringify(res));
 				}
 			});
 	}
