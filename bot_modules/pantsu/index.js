d0a4b822ddecbc162932b8036e28143d1d26e918
David Zech <david.zech@gmail.com>
2014-05-08 22:26:19 -0700

    Abstract imgur out

--- bot_modules/pantsu/index.js
+++ bot_modules/pantsu/index.js
@@ -2,87 +2,22 @@ var https = require('https');
 var querystring = require('querystring');
 var fs = require('fs');
 var utils = require('utils');
+var imgur = require('imgur');
 
 var oauth = require(__dirname + '/oauth.json');
-
-function requestAccessToken(callback)
-{
-	var post_data = querystring.stringify(
-	{
-		'refresh_token' : oauth.refresh_token,
-		'client_id' : oauth.client_id,
-		'client_secret' : oauth.client_secret,
-		'grant_type' : 'refresh_token'
-	});
-
-	var post_options = {
-		host: 'api.imgur.com',
-		path: '/oauth2/token',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-        	'Content-Length': post_data.length
-		}
-	}
-
-	var post_req = https.request(post_options, function(res)
-	{
-		res.setEncoding('utf8');
-		res.on('data', function(data)
-		{
-			callback(JSON.parse(data));
-		})
-	});
-
-	post_req.write(post_data);
-	post_req.end();
-}
-
-exports.requestAccessToken = requestAccessToken;
-
-function uploadImage (url, callback) {
-	var post_data = querystring.stringify({
-		'image' : url,
-		'album' : oauth.album_id,
-		'type' : 'URL'
-	});
-
-	var post_options = {
-		host: 'api.imgur.com',
-		path: '/3/image',
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded',
-        	'Content-Length': post_data.length,
-        	'Authorization' : 'Bearer ' + oauth.access_token
-		}
-	}
-
-	var post_req = https.request(post_options, function(res)
-	{
-		res.setEncoding('utf8');
-		res.on('data', function(data)
-		{
-			callback(JSON.parse(data));
-		})
-	});
-
-	post_req.write(post_data);
-	post_req.end();
-}
 
 function pantsuCommand(irc, from, to, url)
 {
 	if(oauth.access_token == null || oauth.expire_time < Date.now())
 	{
-		requestAccessToken(function(res)
+		imgur.requestAccessToken(oauth.refresh_token, oauth.client_id, oauth.client_secret, function(res)
 		{
 			if(res)
 			{
 				oauth.access_token = res.access_token;
 				oauth.expire_time = Date.now() + (res.expires_in * 1000);
 
-				uploadImage(url, function(res)
+				imgur.uploadImage(url, oauth.album_id, oauth.access_token, function(res)
 					{
 						if(res.success)
 						{
@@ -99,7 +34,7 @@ function pantsuCommand(irc, from, to, url)
 	}
 	else
 	{
-		uploadImage(url, function(res)
+		imgur.uploadImage(url, oauth.album_id, function(res)
 			{
 				if(res.success)
 				{
